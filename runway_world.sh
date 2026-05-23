5748b60cc0ac7fa56fea9b9777fdf896f70482bb
Marek Malina <125477734+marekm256@users.noreply.github.com>
2026-05-23 11:05:35 +0200

    fixed no image_raw in gazebo

--- runway_world.sh
+++ runway_world.sh
@@ -7,6 +7,6 @@ source /opt/ros/humble/setup.bash
 source /home/lrs/ardupilot/Tools/completion/completion.bash
 source "${PROJECT_ROOT}/ros2_ws/install/setup.bash"
 
-export GAZEBO_MODEL_PATH="${GAZEBO_MODEL_PATH}:${PROJECT_ROOT}/World_Dron/model"
+export GAZEBO_MODEL_PATH="${PROJECT_ROOT}/World_Dron/model:${GAZEBO_MODEL_PATH:-}"
 
 gazebo "${PROJECT_ROOT}/World_Dron/worlds/iris_arducopter_runway.world"
